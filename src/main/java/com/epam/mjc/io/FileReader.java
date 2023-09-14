629967b34b3e08562ca3d240c09f261721b22ef5
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-14 15:58:23 +0600

    Task IO commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String str;
+
+            while ((str = br.readLine()) != null) {
+                String[] lineArr = str.split(": ");
+
+                String key = lineArr[0];
+                String value = lineArr[1];
+
+                switch (key) {
+                    case "Name":
+                        profile.setName(value);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(value));
+                        break;
+                    case "Email":
+                        profile.setEmail(value);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(value));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
 }
